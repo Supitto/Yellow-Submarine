49bc022d8011f9c2933635d9daf7124ec1d92b17
andlzanon <andrelzanon10@gmail.com>
2017-01-17 21:09:10 -0200

    Voltando desenha mapa

--- YellowSubmarine.asm
+++ YellowSubmarine.asm
@@ -1,11 +1,7 @@
 INCLUDE Irvine32.inc
-
-desenhasubmarino PROTO, xy : BYTE      ;Prototipo da função desenha submarino
-apagasubmarino PROTO, xy : BYTE        ;Prototipo da função apaga submarino
-
-
+.data
+	char_mapa byte '*'
 .code
-
 main PROC
 	call desenhamapa
 	call waitMsg
@@ -21,7 +17,7 @@ desenhamapa PROC
 
 									   ;// desenha linha de cima 
 L:
-	call Gotoxy						   ;// vai apra a posi��o dh, dl
+	call Gotoxy						   ;// vai apra a posi��o dh, dl
 	push edx						   ;// manda para a pilha o valor de dl e dh
 	mov edx, OFFSET char_mapa		   ;// prepara para desenhar *
 	call WriteString                   ;// escreve *
@@ -29,7 +25,7 @@ L:
 	inc dl                             ;// incrementa valor da coluna
 	loop L
 
-	                                   ;// desenha a coluna mais � direita 
+	                                   ;// desenha a coluna mais � direita 
 	mov dl, 0
 	mov dh, 1
 	mov ecx, 30
@@ -38,33 +34,37 @@ L2:
 	push edx
 	mov edx, OFFSET char_mapa
 	call WriteString
+	pop edx
+	inc dh                             ;// incrementa valor da linha
+	loop L2
+
+			                           ;// desenha a coluna mais � esquerda
+	mov dl, 30						   ;// posicao da ultima * da linha de cima
+	mov dh, 0
+	mov ecx, 30
+L3:
+	call Gotoxy
+	push edx
+	mov edx, OFFSET char_mapa
+	call WriteString
+	pop edx
+	inc dh					           ;// incrementa valor da linha
+	loop L3
+
+	mov dl, 1                          ;// desenha linha de baixo
+	mov dh, 30
+	mov ecx, 30		
+L4:								
+	call Gotoxy
+	push edx
+	mov edx, OFFSET char_mapa
+	call WriteString
+	pop edx
+	inc dl
+	loop L4
+
 	popad
 	call crlf
 	ret
 desenhamapa ENDP
-
-
-desenhasubmarino PROC, xy:BYTE
-.code
-	pushad                             ;//Joga os registradores na pilha
-	mov al, 'S'                        ;//Guarda o S no al para ser desenhado
-	mov dx, xy                         ;//Move o x para dh e o y par dl
-	call Gotoxy						   ;//Move o cursor
-	call WriteChar                     ;//Desenha o submarino
-	popad							   ;//Retorna os registradores da pilha
-	ret								   ;//Fim da função
-desenhasubmarino ENDP
-
-apagasubmarino PROC, xy : BYTE
-.code
-	pushad                             ;//Joga os registradores na pilha
-	mov al, ' '                        ;//Guarda o S no al para ser desenhado
-	mov dx, xy                         ;//Move o x para dh e o y par dl
-	call Gotoxy						   ;//Move o cursor
-	call WriteChar                     ;//Desenha o submarino
-	popad							   ;//Retorna os registradores da pilha
-	ret								   ;//Fim da função
-apagasubmarino ENDP
-
-
 END main
