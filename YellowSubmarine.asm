e6b25dd7fef96b583b18662d5f92eb7157391260
andlzanon <andrelzanon10@gmail.com>
2017-01-17 20:50:44 -0200

    Função Desenha Mapa _semBug

--- YellowSubmarine.asm
+++ YellowSubmarine.asm
@@ -1,7 +1,6 @@
 INCLUDE Irvine32.inc
 .data
-	sup_mapa byte '******************************************************************************'
-	lado_mapa byte'*                                                                            *'
+	char_mapa byte '*'
 .code
 main PROC
 	call desenhamapa
@@ -12,19 +11,60 @@ main ENDP
 desenhamapa PROC
 .code
 	pushad
-	mov edx, OFFSET sup_mapa
-	call WriteString
-	call crlf
-	mov edx, OFFSET lado_mapa
-	mov ecx, 30
+	mov dl, 0						   ;// coluna inicial para desenhar a linha de cima do tabuleiro
+	mov dh, 0						   ;// linha inicial para desenhar a linha de cima do tabuleiro
+	mov ecx, 30						   ;// numero de linhas
+
+									   ;// desenha linha de cima 
 L:
-	call WriteString
-	call crlf
+	call Gotoxy						   ;// vai apra a posição dh, dl
+	push edx						   ;// manda para a pilha o valor de dl e dh
+	mov edx, OFFSET char_mapa		   ;// prepara para desenhar *
+	call WriteString                   ;// escreve *
+	pop edx                            ;// volta para a pilha valor de dl e dh
+	inc dl                             ;// incrementa valor da coluna
 	loop L
-	mov edx, OFFSET sup_mapa
+
+	                                   ;// desenha a coluna mais à direita 
+	mov dl, 0
+	mov dh, 1
+	mov ecx, 30
+L2:
+	call Gotoxy
+	push edx
+	mov edx, OFFSET char_mapa
 	call WriteString
-	call crlf
+	pop edx
+	inc dh                             ;// incrementa valor da linha
+	loop L2
+
+			                           ;// desenha a coluna mais à esquerda
+	mov dl, 30						   ;// posicao da ultima * da linha de cima
+	mov dh, 0
+	mov ecx, 30
+L3:
+	call Gotoxy
+	push edx
+	mov edx, OFFSET char_mapa
+	call WriteString
+	pop edx
+	inc dh					           ;// incrementa valor da linha
+	loop L3
+
+	mov dl, 1                          ;// desenha linha de baixo
+	mov dh, 30
+	mov ecx, 30		
+L4:								
+	call Gotoxy
+	push edx
+	mov edx, OFFSET char_mapa
+	call WriteString
+	pop edx
+	inc dl
+	loop L4
+
 	popad
+	call crlf
 	ret
 desenhamapa ENDP
 END main
