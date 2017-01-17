9a71bef331101a58e824a370dbed07a25d7a9a15
Supitto <henriquemarcomini@live.com>
2017-01-17 20:03:26 -0200

    apa_bug

--- YellowSubmarine.asm
+++ YellowSubmarine.asm
@@ -1,11 +1,30 @@
 INCLUDE Irvine32.inc
-
 .data
-	
+	sup_mapa byte '******************************************************************************'
+	lado_mapa byte'*                                                                            *'
 .code
 main PROC
-	
+	call desenhamapa
 	call waitMsg
 	exit
 main ENDP
+
+desenhamapa PROC
+.code
+	pushad
+	mov edx, OFFSET sup_mapa
+	call WriteString
+	call crlf
+	mov edx, OFFSET lado_mapa
+	mov ecx, 30
+L:
+	call WriteString
+	call crlf
+	loop L
+	mov edx, OFFSET sup_mapa
+	call WriteString
+	call crlf
+	popad
+	ret
+desenhamapa ENDP
 END main
